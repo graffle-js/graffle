f76867b4ba654720ed88906c18449b1c0b2abe7a
Jason Kuhrt <jasonkuhrt@me.com>
2024-10-31 09:22:54 -0400

    feat(client): scalar statically validate sddm present

---
+++ src/layers/6_client/chainExtensions/scalar.test-d.ts
@@ -0,0 +1,11 @@
+import { schemaDrivenDataMap } from '../../../../tests/_/schemas/kitchen-sink/graffle/modules/schema-driven-data-map.js'
+import { Graffle } from '../../../entrypoints/__Graffle.js'
+import { assertEqual } from '../../../lib/assert-equal.js'
+import type { SomeFunction } from '../../../lib/prelude.js'
+import type { TypeErrorMissingSchemaMap } from './scalar.js'
+
+const g1 = Graffle.create({ schema: `foo` })
+assertEqual<typeof g1.scalar, TypeErrorMissingSchemaMap>()
+
+const g2 = Graffle.create({ schema: `foo`, schemaMap: schemaDrivenDataMap })
+assertEqual<typeof g2.scalar, SomeFunction>()
