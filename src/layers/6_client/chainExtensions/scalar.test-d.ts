1f2e9d06b81404aec788fce3532acaddbcb1abc7
Jason Kuhrt <jasonkuhrt@me.com>
2024-10-31 09:24:34 -0400

    refactor

--- src/layers/6_client/chainExtensions/scalar.test-d.ts
+++ src/layers/6_client/chainExtensions/scalar.test-d.ts
@@ -1,4 +1,4 @@
-import { schemaDrivenDataMap } from '../../../../tests/_/schemas/kitchen-sink/graffle/modules/schema-driven-data-map.js'
+import { schemaMap } from '../../../../tests/_/schemas/kitchen-sink/graffle/__.js'
 import { Graffle } from '../../../entrypoints/__Graffle.js'
 import { assertEqual } from '../../../lib/assert-equal.js'
 import type { SomeFunction } from '../../../lib/prelude.js'
@@ -7,5 +7,5 @@ import type { TypeErrorMissingSchemaMap } from './scalar.js'
 const g1 = Graffle.create({ schema: `foo` })
 assertEqual<typeof g1.scalar, TypeErrorMissingSchemaMap>()
 
-const g2 = Graffle.create({ schema: `foo`, schemaMap: schemaDrivenDataMap })
+const g2 = Graffle.create({ schema: `foo`, schemaMap })
 assertEqual<typeof g2.scalar, SomeFunction>()
