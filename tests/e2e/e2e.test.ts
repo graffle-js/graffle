9e01cac0052b7e7d60570e461a6ef0af394adf1d
Jason Kuhrt <jasonkuhrt@me.com>
2024-11-11 13:16:44 -0500

    fix

--- tests/e2e/e2e.test.ts
+++ tests/e2e/e2e.test.ts
@@ -17,89 +17,89 @@ import { test } from '../_/helpers.js'
 
 // ...?
 
-// test(`client works without generation`, async ({ project, pokemonService }) => {
-//   await project.fs.writeAsync(
-//     `main.ts`,
-//     `
-// 		import { Graffle } from 'graffle'
-// 		const graffle = Graffle.create({ schema: '${pokemonService.url.href}' })
-// 		const data = await graffle.gql\`
-//       query ($name: String!) {
-//         pokemonByName (name: $name) {
-//           name
-//           hp
-//           attack
-//           defense
-//           trainer {
-//             name
-//           }
-//         }
-//       }
-//     \`.send({ name: 'Pikachu' })
-// 		console.log(data?.pokemonByName)
-// 		`,
-//   )
-//   const result = await project.run`pnpm tsx main`
-//   expect(result.stdio).toMatchSnapshot()
-// })
+test(`client works without generation`, async ({ project, pokemonService }) => {
+  await project.fs.writeAsync(
+    `main.ts`,
+    `
+		import { Graffle } from 'graffle'
+		const graffle = Graffle.create({ schema: '${pokemonService.url.href}' })
+		const data = await graffle.gql\`
+      query ($name: String!) {
+        pokemonByName (name: $name) {
+          name
+          hp
+          attack
+          defense
+          trainer {
+            name
+          }
+        }
+      }
+    \`.send({ name: 'Pikachu' })
+		console.log(data?.pokemonByName)
+		`,
+  )
+  const result = await project.run`pnpm tsx main`
+  expect(result.stdio).toMatchSnapshot()
+})
 
-// test(`client works with generation`, async ({ project, pokemonService }) => {
-//   await project.fs.writeAsync(
-//     `main.ts`,
-//     `
-// 		import { Graffle } from 'graffle'
-// 		const graffle = Graffle.create({ schema: '${pokemonService.url.href}' })
-// 		const data = await graffle.query.pokemonByName({
-// 			$: { name: 'Pikachu' },
-// 			name: true,
-// 			hp: true,
-// 			attack: true,
-// 			defense: true,
-// 			trainer: {
-// 				name: true,
-// 			},
-// 		})
-// 		console.log(data)
-// 		`,
-//   )
-//   {
-//     const result = await project.run`pnpm graffle --schema http://localhost:3001/graphql`
-//     expect(result.stdio).toMatchSnapshot()
-//   }
-//   {
-//     await project.run`pnpm check:types`
-//   }
-//   {
-//     const result = await project.run`pnpm tsx main`
-//     expect(result.stdio).toMatchSnapshot()
-//   }
-// })
+test(`client works with generation`, async ({ project, pokemonService }) => {
+  await project.fs.writeAsync(
+    `main.ts`,
+    `
+		import { Graffle } from 'graffle'
+		const graffle = Graffle.create({ schema: '${pokemonService.url.href}' })
+		const data = await graffle.query.pokemonByName({
+			$: { name: 'Pikachu' },
+			name: true,
+			hp: true,
+			attack: true,
+			defense: true,
+			trainer: {
+				name: true,
+			},
+		})
+		console.log(data)
+		`,
+  )
+  {
+    const result = await project.run`pnpm graffle --schema http://localhost:3001/graphql`
+    expect(result.stdio).toMatchSnapshot()
+  }
+  {
+    await project.run`pnpm check:types`
+  }
+  {
+    const result = await project.run`pnpm tsx main`
+    expect(result.stdio).toMatchSnapshot()
+  }
+})
 
-// test(`client uses dprint formatter if installed`, async ({ project }) => {
-//   await project.addDprintConfig()
-//   const path = await project.addPokemonSchemaSDL()
+test(`client uses dprint formatter if installed`, async ({ project }) => {
+  await project.addDprintConfig()
+  const path = await project.addPokemonSchemaSDL()
 
-//   await project.run`pnpm add --save-dev dprint @dprint/formatter @dprint/typescript`
+  await project.run`pnpm add --save-dev dprint @dprint/formatter @dprint/typescript`
 
-//   const genResult = await project.run`pnpm graffle --schema ${path.relative} --format`
-//   const genResultStdout = genResult.stdout as string
-//   expect(genResultStdout.includes(`No TypeScript formatter found`)).toEqual(false)
+  const genResult = await project.run`pnpm graffle --schema ${path.relative} --format`
+  const genResultStdout = genResult.stdout as string
+  expect(genResultStdout.includes(`No TypeScript formatter found`)).toEqual(false)
 
-//   await project.run`pnpm dprint check graffle/**/*`
-// })
+  await project.run`pnpm dprint check graffle/**/*`
+})
 
-// test(`client uses prettier formatter if installed`, async ({ project }) => {
-//   // await project.addDprintConfig()
-//   const path = await project.addPokemonSchemaSDL()
+test(`client uses prettier formatter if installed`, async ({ project }) => {
+  // await project.addDprintConfig()
+  const path = await project.addPokemonSchemaSDL()
 
-//   await project.run`pnpm add --save-dev prettier`
+  await project.run`pnpm add --save-dev prettier`
 
-//   const genResult = await project.run`pnpm graffle --schema ${path.relative} --format`
-//   const genResultStdout = genResult.stdout as string
-//   expect(genResultStdout.includes(`No TypeScript formatter found`)).toEqual(false)
+  const genResult = await project.run`pnpm graffle --schema ${path.relative} --format`
+  const genResultStdout = genResult.stdout as string
+  expect(genResultStdout.includes(`No TypeScript formatter found`)).toEqual(false)
 
-//   await project.run`pnpm prettier --check graffle/**/*`
-// })
+  await project.run`pnpm prettier --check graffle/**/*`
+})
 
 test(`project uses graffle config file if present`, async ({ project, onTestFinished }) => {
   // Clean up global side effects caused by this e2e test in development.
