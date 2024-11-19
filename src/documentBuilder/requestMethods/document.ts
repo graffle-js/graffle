cc3873f28842cfe51fbd03574e0c088d5d5fe107
Jason Kuhrt <jasonkuhrt@me.com>
2024-11-18 23:07:27 -0500

    improve(document-builder): deeply simplify results

--- src/documentBuilder/requestMethods/document.ts
+++ src/documentBuilder/requestMethods/document.ts
@@ -4,7 +4,7 @@ import { type HandleOutput } from '../../client/handleOutput.js'
 import type { InferResult } from '../../documentBuilder/InferResult/__.js'
 import type { Select } from '../../documentBuilder/Select/__.js'
 import type { Schema } from '../../entrypoints/schema.js'
-import type { IsTupleMultiple } from '../../lib/prelude.js'
+import type { IsTupleMultiple, SimplifyDeep, SimplifyDeepExcept } from '../../lib/prelude.js'
 
 // dprint-ignore
 export type DocumentRunner<
@@ -18,7 +18,8 @@ export type DocumentRunner<
     const $Name extends string = $Params extends [] ? $$Name : $Params[0],
   >(...params: $Params) =>
     Promise<
-      Simplify<
+      SimplifyDeepExcept<
+        any, // todo custom scalar types
         HandleOutput<
           $$Context,
           InferResult.Operation<
