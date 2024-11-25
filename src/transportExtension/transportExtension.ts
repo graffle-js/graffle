9f067163ac8c1348ce175238e72d873f7a397f07
Jason Kuhrt <jasonkuhrt@me.com>
2024-11-25 10:25:46 -0500

    fix

--- src/transportExtension/transportExtension.ts
+++ src/transportExtension/transportExtension.ts
@@ -1,17 +1,17 @@
 import type { Anyware } from '../lib/anyware/__.js'
 import { __ } from '../lib/prelude.js'
-import type { RequestPipelineBaseContext } from '../requestPipeline/RequestPipeline.js'
+import type { RequestPipelineDefinition } from '../requestPipeline/RequestPipeline.js'
 
-export interface TransportExtension extends Anyware.Overload.BuilderContext {
+export interface TransportExtension extends Anyware.Overload {
   // name: $Name
   // discriminant: ['transportType', $Name]
 }
 
-export interface TransportExtensionInitial
+// export interface TransportExtensionInitial
 
 export namespace TransportExtension {
   interface Create {
-    <$Name extends string>(name: $Name): Anyware.Overload.Builder<RequestPipelineBaseContext, {
+    <$Name extends string>(name: $Name): Anyware.Overload.Builder<RequestPipelineDefinition, {
       discriminant: ['transportType', $Name]
       input: {}
       steps: {}
