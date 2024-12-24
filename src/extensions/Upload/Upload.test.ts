5e5fb1f98bb592de64cf40b1b2009dfcfddf0e96
Jason Kuhrt <jasonkuhrt@me.com>
2024-12-24 14:44:01 -0500

    refactor

--- src/extensions/Upload/Upload.test.ts
+++ src/extensions/Upload/Upload.test.ts
@@ -1,7 +1,7 @@
 // todo in order to test jsdom, we need to boot the server in a separate process
 // @vitest-environment node
 
-import { afterAll, beforeAll, beforeEach, expect, test } from 'vitest'
+import { expect, test as testBase } from 'vitest'
 import * as UploadSchema from '../../../tests/_/schemas/upload/schema.js'
 import { Graffle } from '../../entrypoints/main.js'
 import { Upload } from './Upload.js'
@@ -9,24 +9,24 @@ import { Upload } from './Upload.js'
 import { type SchemaService, serveSchema } from '../../../tests/_/lib/serveSchema.js'
 import type { GraffleMinimal } from '../../entrypoints/presets/minimal.js'
 
-let schemaServer: SchemaService
+interface Context {
+  graffle: GraffleMinimal.Client.With<{ checkPreflight: false }>
+  schemaServer: SchemaService
+}
 
-let graffle: GraffleMinimal.Client.With<{ checkPreflight: false }>
-
-beforeAll(async () => {
-  schemaServer = await serveSchema({ schema: UploadSchema.schema })
+const test = testBase.extend<Context>({
+  schemaServer: async ({}, use) => { // eslint-disable-line
+    const schemaServer = await serveSchema({ schema: UploadSchema.schema })
+    await use(schemaServer)
+    await schemaServer.stop()
+  },
+  graffle: async ({ schemaServer }, use) => {
+    const graffle = Graffle.create({ checkPreflight: false }).transport({ url: schemaServer.url }).use(Upload())
+    await use(graffle as any)
+  },
 })
 
-beforeEach(() => {
-  const graffle_ = Graffle.create({ checkPreflight: false }).transport({ url: schemaServer.url }).use(Upload())
-  graffle = graffle_ as any
-})
-
-afterAll(async () => {
-  await schemaServer.stop()
-})
-
-test(`upload`, async () => {
+test(`upload`, async ({ graffle }) => {
   const data = await graffle.gql`
     mutation ($blob: Upload!) {
       readTextFile(blob: $blob)
@@ -41,7 +41,7 @@ test(`upload`, async () => {
   `)
 })
 
-test(`client with upload extension making non-upload request`, async () => {
+test(`client with upload extension making non-upload request`, async ({ graffle }) => {
   const data = await graffle.gql`
     query {
       greetings
