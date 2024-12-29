d731c30a147d419fdcb77a36415a00209f276bbb
Jason Kuhrt <jasonkuhrt@me.com>
2024-12-29 13:31:03 -0500

    feat(extension/document-builder): make document-builder an extension

--- src/entrypoints/presets/_basic.ts
+++ src/entrypoints/presets/_basic.ts
@@ -2,11 +2,15 @@ import { type Client as BaseClient, createConstructorWithContext } from '../../c
 import { useReducer } from '../../client/properties/use.js'
 import type { ConfigManager } from '../../lib/config-manager/__.js'
 import { Context, type Context as BaseContext } from '../../types/context.js'
+import { DocumentBuilder } from '../extensions/document-builder/runtime.js'
 import { TransportHttp } from '../extensions/transport-http/runtime.js'
 import { TransportMemory } from '../extensions/transport-memory/runtime.js'
 import type { PartialOrUndefined } from '../main.js'
 
-const context = useReducer(useReducer(Context.States.empty, TransportHttp()), TransportMemory())
+const context = useReducer(
+  useReducer(useReducer(Context.States.empty, TransportHttp()), TransportMemory()),
+  DocumentBuilder(),
+)
 
 type BasicClientContext = typeof context
 
