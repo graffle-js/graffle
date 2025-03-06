96a125bfbbb6539212d0c7ca97265a690ff2f2c7
Jason Kuhrt <jasonkuhrt@me.com>
2025-03-06 11:37:45 -0500

    make more self-contained

--- src/generator/config/config.test.ts
+++ src/generator/config/config.test.ts
@@ -1,8 +1,13 @@
+import * as Memfs from 'memfs'
+import * as Path from 'node:path'
 import { expect } from 'vitest'
 import { describe } from 'vitest'
 import { test } from '../../../tests/_/helpers.js'
+import { type Fs, writeFileAndCreateDir } from '../../lib/fsp.js'
 import { createConfig } from './config.js'
 import type { ConfigInitSchemaSdl } from './configInit.js'
+
+const fs = Memfs.fs.promises as any as Fs
 
 const schema: ConfigInitSchemaSdl = {
   type: `sdl`,
@@ -16,7 +21,10 @@ describe(`import format`, () => {
   })
   test(`noExtension`, async () => {
     const customPathFile = `./tests/_/fixtures/custom.graphql`
+    await fs.mkdir(Path.dirname(customPathFile), { recursive: true })
+    await fs.writeFile(customPathFile, `type Query { ok: Boolean }`)
     const config = await createConfig({
+      fs,
       schema: { type: `sdlFile`, dirOrFilePath: customPathFile },
       importFormat: `noExtension`,
     })
@@ -26,7 +34,9 @@ describe(`import format`, () => {
 
 test(`can load schema from custom path`, async () => {
   const customPathFile = `./tests/_/fixtures/custom.graphql`
-  const config = await createConfig({ schema: { type: `sdlFile`, dirOrFilePath: customPathFile } })
+  await writeFileAndCreateDir(fs, customPathFile, `type Query { customNamedSchemaFile: Boolean }`)
+
+  const config = await createConfig({ fs, schema: { type: `sdlFile`, dirOrFilePath: customPathFile } })
   const field = config.schema.instance.getQueryType()?.getFields()[`customNamedSchemaFile`]
   expect(config.paths.project.inputs.schema).match(new RegExp(customPathFile + `$`))
   expect(config.schema.sdl).toMatchSnapshot()
@@ -35,9 +45,12 @@ test(`can load schema from custom path`, async () => {
 
 test(`can load schema from custom dir using default file name`, async () => {
   const customPathDir = `tests/_/fixtures`
-  const config = await createConfig({ schema: { type: `sdlFile`, dirOrFilePath: customPathDir } })
+  const schemaFilePath = Path.join(customPathDir, `schema.graphql`)
+  await writeFileAndCreateDir(fs, schemaFilePath, `type Query { defaultNamedSchemaFile: Boolean }`)
+
+  const config = await createConfig({ fs, schema: { type: `sdlFile`, dirOrFilePath: customPathDir } })
   const field = config.schema.instance.getQueryType()?.getFields()[`defaultNamedSchemaFile`]
-  expect(config.paths.project.inputs.schema).match(new RegExp(customPathDir + `/schema.graphql$`))
+  expect(config.paths.project.inputs.schema).match(new RegExp(schemaFilePath + `$`))
   expect(config.schema.sdl).toMatchSnapshot()
   expect(field).toBeDefined()
 })
