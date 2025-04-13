d208da8421da580758e7d7e2e42b990bb87a49e3
Jason Kuhrt <jasonkuhrt@me.com>
2025-04-13 19:48:11 -0400

    fix output simplify

--- src/extensions/Introspection/Introspection.test.ts
+++ src/extensions/Introspection/Introspection.test.ts
@@ -7,6 +7,7 @@ import { Introspection } from './Introspection.js'
 
 test(`adds an introspection method that introspects the schema`, async ({ schemas: { minimal: schema } }) => {
   const graffle = GraffleBasic.create().transport(`memory`, { schema }).use(Introspection())
+  graffle._.typeHookRequestResultDataTypes
   const result = await graffle.introspect()
   expect(result).toMatchSnapshot()
   assertEqual<typeof result, IntrospectionQuery | null>()
