0714e510374de2be3b9b589f92d0e98b852ed606
Jason Kuhrt <jasonkuhrt@me.com>
2025-10-13 22:14:34 -0400

    refactor(docpar): move static builders and unified gql to docpar
    Moves document building types and runtime to proper locations:
    
    **docpar/object/static/rootType.ts** (object-only static builders):
    - Config, StaticDocumentBuilder types
    - createStaticRootType/Field functions
    - defaults constant
    - Powers the `.query.user()` API used by generators
    
    **docpar/gql.ts** (unified gql function - bridges string + object):
    - ParseGraphQLString/Object types
    - InferOperations utilities
    - gql interface + createGql function
    - Accepts both full GraphQL strings and full document objects
    
    **Consolidations**:
    - Deleted src/static/rootField.ts (duplicate of static builder in gql.ts)
    - Deleted src/extensions/DocumentBuilder/staticBuilderDefaults.ts (moved to rootType.ts)
    - src/static/gql.ts now re-exports from new locations for backward compat
    
    Static builders are object-only (string partial syntax not viable due to LSP/autocomplete limitations).
    Unified gql works with full documents in either mode.

--- src/exports/extensions/document-builder/runtime.ts
+++ src/exports/extensions/document-builder/runtime.ts
@@ -1,11 +1,10 @@
-export { Var } from '#src/docpar/object/var/$.js'
-export { DocumentBuilder } from '#src/extensions/DocumentBuilder/DocumentBuilder.js'
+export { createGql, type InferOperations } from '#src/docpar/gql.js'
 export {
   type Config,
-  createGql,
   createStaticRootType,
   defaults as staticBuilderDefaults,
-  type InferOperations,
   type StaticDocumentBuilder,
-} from '#src/static/gql.js'
-export { $ } from '../../../docpar/object/var/var.js'
+} from '#src/docpar/object/static/rootType.js'
+export { Var } from '#src/docpar/object/var/$.js'
+export { $ } from '#src/docpar/object/var/var.js'
+export { DocumentBuilder } from '#src/extensions/DocumentBuilder/DocumentBuilder.js'
