92e27e67189a4b6cc7285ad275eff4af69f44862
Jason Kuhrt <jasonkuhrt@me.com>
2025-10-13 22:20:24 -0400

    refactor: remove src/static/ and update imports to use docpar
    Delete src/static/ directory entirely as its functionality has been moved to src/docpar/:
    - src/static/gql.ts → src/docpar/gql.ts (unified gql function)
    - src/static/rootField.ts → src/docpar/object/static/rootType.ts (static builders)
    - src/extensions/DocumentBuilder/staticBuilderDefaults.ts → consolidated into docpar/object/static/rootType.ts
    
    Update all consumers to import directly from docpar:
    - src/client/methods/gql/gql.ts: import ParseGraphQL* from docpar/gql.js
    - src/exports/utilities-for-generated.ts: export gql type from docpar/gql.js
    
    Remove tests and snapshots that belonged to src/static/:
    - gql.test.ts, gql.test-d.ts, rootField.test.ts
    - __snapshots__/gql.test.ts.snap
    
    These tests will be maintained in the docpar directory structure.

--- src/extensions/DocumentBuilder/staticBuilderDefaults.ts
+++
@@ -1,5 +0,0 @@
-import type { Options } from '#src/docpar/object/ToGraphQLDocument/nodes/1_Document.js'
-
-export const defaults: Partial<Options> = {
-  hoistArguments: true,
-}
--- src/static/gql.ts
+++
@@ -1,8 +0,0 @@
-// Re-exports for backward compatibility
-// Static builders moved to docpar/object/static/
-export type { Config, StaticDocumentBuilder } from '#src/docpar/object/static/rootType.js'
-export { createStaticRootField, createStaticRootType, defaults } from '#src/docpar/object/static/rootType.js'
-
-// Unified gql function moved to docpar root
-export type { gql, InferOperations, ParseGraphQLObject, ParseGraphQLString } from '#src/docpar/gql.js'
-export { createGql, Gql } from '#src/docpar/gql.js'
