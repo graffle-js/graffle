fd1bdadc83061aad1026265de588f8c5b730cadc
Jason Kuhrt <jasonkuhrt@me.com>
2025-11-03 10:37:37 -0500

    refactor: organize kind utilities under schema/kind namespace
    Consolidates all kind-related utilities (TypeKind, KindMap, getTypeAndKind) under schema/kind/ namespace.
    
    Changes:
    - Create schema/kind/ directory
    - Move KindMap/ → kind/KindMap/
    - Move TypeKind and utilities to kind/__.ts
    - Export as Schema.Kind namespace
    - Update all imports in generators
    
    Benefits:
    - All kind concepts in one place
    - Clear namespace organization
    - Easier to find kind-related functionality
    
    Related to #1434

--- src/lib/graphql-kit/schema/__.ts
+++ src/lib/graphql-kit/schema/__.ts
@@ -1,9 +1,8 @@
 export { buildClientSchema, buildSchema, getNamedType, getNullableType, printSchema as print } from 'graphql'
 export * from './kind/_.js'
-// export * from './kind/enum.js' // todo remove
 export * from './runtime/_.js'
 export * from './scalars/_.js'
-export * from './scalars/__.js' // todo remove
+// export * from './scalars/__.js' // todo remove
 export * as CustomScalars from './scalars/customScalars.js'
-export * from './scalars/scalars.js' // todo remove
+// export * from './scalars/scalars.js' // todo remove
 export * from './scalars/typeGuards.js' // todo remove
